c2074f12c2065b5ce1ab06b5668283d61b49e62c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-11-26 21:32:40 -0500

    [change] Create hello world angular

--- catalogo/src/main/frontend/src/app/app.component.ts
+++ catalogo/src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+@Injectable()
 export class AppComponent {
   title = 'frontend';
+  result = '';
+
+  constructor(private http: HttpClient){
+  }
+
+  sayHello(): void {
+    this.result = 'loading...';
+    this.http.get('/api/hello-world').subscribe(response => this.result = response.toString());
+  }
 }
