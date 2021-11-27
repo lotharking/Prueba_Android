13816669406a775eb649ca454313b0d76d630a72
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-11-27 11:20:33 -0500

    [change] contact component, history and 404 not found

--- catalogo/src/main/frontend/src/app/app-routing.module.ts
+++ catalogo/src/main/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './components/car/car.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { DetailsProductComponent } from './components/details-product/details-product.component';
 import { Page404Component } from './components/page404/page404.component';
+import { HistoryComponent } from './components/users/history/history.component';
 
 
 /**Rutas de la App*/
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: CarComponent },
+  { path: '', component: CarComponent },
   // { path: 'car/:id', component: DetailsProductComponent},
-  { path: 'car/1', component: DetailsProductComponent},
+  { path: 'compras/1', component: DetailsProductComponent},
+  { path: 'contactenos', component: ContactComponent},
+  { path: 'historial/1', component: HistoryComponent},
   { path: '**', component: Page404Component},
 ];
 //end routing
