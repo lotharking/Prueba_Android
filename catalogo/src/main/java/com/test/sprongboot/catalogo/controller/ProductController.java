31fba4eaee1cd3360e140f8dab4ef86152fe88e8
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-11-27 23:54:58 -0500

    [create] create DB data

--- catalogo/src/main/java/com/test/sprongboot/catalogo/controller/ProductController.java
+++ catalogo/src/main/java/com/test/sprongboot/catalogo/controller/ProductController.java
@@ -1,55 +1,38 @@
 package com.test.sprongboot.catalogo.controller;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import com.test.sprongboot.catalogo.entity.Product;
 import com.test.sprongboot.catalogo.repository.ProductRepository;
 
+// import org.h2.util.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 // import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/productos")
 public class ProductController {
-  // @GetMapping(path = "/api/hello-world")
-  // public String helloWorld(){
-  //   return "Hello world!";
-  // }
+
   @Autowired
   private ProductRepository repository;
 
   
-  @GetMapping(path = "/api/hello-world")
-  public String helloWorld(){
-    
-    repository.save( new Product("Papel", 20000, 20));
-    // String[][] data = {
-    //   		{"Papel", "20000", "20"},
-    //   		{"Locion", "30000", "100"},
-    //   		{"Cartas", "25000", "75"}
-    //   	};
-    //     Stream.of(data).forEach(array -> {
-    //       		  try {
-    //       			Product product = new Product(
-    //       				array[0],
-    //       				Integer.valueOf(array[1]),
-    //       				NumberFormat.getInstance().parse(array[2])
-    //       			);
-    //       			repository.save(product);
-    //       		  }
-    //       		  catch (ParseException e) {
-    //       			e.printStackTrace();
-    //       		  }
-    //       		});
-    // return repository.findAll().toString();
-    return "Hello world";
+  @GetMapping(path = "/lista")
+  public String list(){
+   
+    List<Product> list = new ArrayList<>();
+    repository.findAll().forEach(list::add);
+
+    return list.toString();
+    // return "Hello world";
   }
 }
---
+++ catalogo/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- Delete database
+delete from product where id > 0;
+
+-- Insert data
+INSERT INTO product (name, price, stock) VALUES
+  ('casa', 200, 5),
+  ('carro', 300, 10),
+  ('beca', 100, 12),
+  ('mina', 1000, 15),
+  ('sal', 1002, 20),
+  ('locion', 500, 25);
