48dc515fde9ac2131069766801910fa7ce610cfd
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-01-30 18:16:54 -0500

    [create] Create user model

---
+++ catalogo/src/main/frontend/src/app/models/user.module.ts
@@ -0,0 +1,10 @@
+export class User {
+  username: string;
+  password: string;
+
+  constructor() { 
+      // Initialization inside the constructor
+      this.username = "";
+      this.password = "";
+   }
+}
