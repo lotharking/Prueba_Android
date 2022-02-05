3e6cd6d9795c53a738bbf8a6454f1e1ff137d2d4
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-02-05 12:17:39 -0500

    [change] create user consult unique

--- catalogo/src/main/frontend/src/app/services/users/users.service.ts
+++ catalogo/src/main/frontend/src/app/services/users/users.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  users = 'http://localhost:8080/users/';
+  header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
+
+  constructor(private httpClient: HttpClient) { }
+
+  /**HttpRequest user unique */
+  public search(value: string): Observable<any> {
+    return this.httpClient.get<any>(this.users + `search/${value}`);
+  }
 }
