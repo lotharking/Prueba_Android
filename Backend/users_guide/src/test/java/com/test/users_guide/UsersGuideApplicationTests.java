ccd51320356c0b5bd27226999616b3b0dab71494
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-03-15 20:56:22 -0500

    [create] add users app for microservices test

---
+++ Backend/users_guide/src/test/java/com/test/users_guide/UsersGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.users_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
