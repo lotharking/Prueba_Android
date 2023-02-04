767ffb4bb0a8f1722a0e2b5bd109fcabf213367a
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-02-03 20:01:52 -0500

    no message

--- Frontend/src/app/components/car/car.component.ts
+++ Frontend/src/app/components/car/car.component.ts
@@ -22,6 +22,7 @@ export class CarComponent implements OnInit {
               private router: Router
               ) { }
 
+  /**Load init */
   ngOnInit(): void {
     this.uploadProducts();
     this.checkWishes();
