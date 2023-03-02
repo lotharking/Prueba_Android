9371f1e95f7c15d8603a95362cdafd4ec6b73993
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-03-01 21:22:10 -0500

    create frontend list categories

--- Frontend/src/app/services/category/category.service.ts
+++ Frontend/src/app/services/category/category.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  
+  categories = 'http://localhost:8080/categories/';
+  header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }  
+
+  /**HttpRequest list products */
+  public list(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.categories + 'list', 
+          { headers : this.header });
+  }
 }
