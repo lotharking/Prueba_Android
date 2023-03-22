88573201373e899d1d7222e6523591bdaebde56f
Andres Vallecilla <andresfelipe083195@hotmail.com>
2023-03-22 01:00:55 -0500

    read country

--- Frontend/src/app/components/navbarbegin/navbarbegin.component.ts
+++ Frontend/src/app/components/navbarbegin/navbarbegin.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/product/products.service';
 
@@ -8,9 +9,13 @@ import { ProductsService } from 'src/app/services/product/products.service';
 })
 export class NavbarbeginComponent implements OnInit {
 
-  constructor(private productsservice: ProductsService,) { }
+  pais: String = '';
+
+  constructor(private productsservice: ProductsService,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getCountry();
   }
 
   filter(value: String): void {  
@@ -19,6 +24,16 @@ export class NavbarbeginComponent implements OnInit {
         this.productsservice.updateResultList(data);
       }
     );
+  }
+
+  getCountry(): void {
+    fetch('https://api.ipregistry.co/?key=tryout')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (payload) {
+        console.log(payload.location.country.name + ', ' + payload.location.city);
+    });
   }
   
 
